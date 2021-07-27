9b66f10e234e3ec9e6880634fa4a594ca28732d9
Shirley Wang <qianqi.wang@uwaterloo.ca>
2021-07-27 19:01:02 +0000

    Update block.h

--- block.h
+++ block.h
@@ -1,1 +1,37 @@
+#ifndef _BLOCK_H
+#define _BLOCK_H
 
+#include <iostream>
+#include <memory>
+#include <vector>
+#include <string>
+using namespace std;
+
+
+class Block {
+    protected:
+    unsigned int xCoord;
+    unsigned int yCoord;
+    unsigned int rotateAngle;
+    bool heavy;
+    unsigned int heavyLevel;
+    
+    public:
+    Block(size_t x, size_t y, size_t angle, bool isHeavy, size_t heavyLevel);
+    ~Block();
+    
+    size_t getXcoord();
+    size_t getYcoord();
+    size_t getAngle();
+    bool isHeavy();
+    size_t getHeavyLevel();
+    
+    void setXcoord();
+    void setYcoord();
+    void setAngle();
+    void setHeavy(size_t heavyLevel);
+    
+};
+
+
+#endif
