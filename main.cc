8f9933bee02b64d92d829e731a067e56b675694d
Xiyu Chen <x789chen@uwaterloo.ca>
2021-07-27 03:10:08 +0000

    Update main.cc

--- main.cc
+++ main.cc
@@ -1,1 +1,13 @@
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+int main(int argc, char** argv) {
+    if (argc < 3) {
+        std::cerr << "need more sequence.txt" << std::endl;
+        return 1;
+    }
+
+    return 0;
+}
