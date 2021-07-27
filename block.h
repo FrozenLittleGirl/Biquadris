b0528ea944e8a519d6b9ebbee03288e0a5f49967
Shirley Wang <qianqi.wang@uwaterloo.ca>
2021-07-27 21:07:10 +0000

    Update block.h

--- block.h
+++ block.h
@@ -8,28 +8,31 @@
 
 class Block {
     protected:
-    unsigned int xCoord;
-    unsigned int yCoord;
-    unsigned int rotateAngle;
-    bool heavy;
-    unsigned int howHeavy;
+    size_t xCoord;         // x coordinate of the top left piece of the block
+    size_t yCoord;         // y coordinate of the top left piece of the block
+    size_t rotateAngle;    // angle of rotation of the block, can only be 4 values:
+                           // 0, 90, 180, 270
+    bool heavy;            // true if the current block is affect by the "heavy" effect
+    size_t howHeavy;       // 
     
     public:
-    Block(size_t x, size_t y, size_t angle, bool isHeavy, size_t heavyCount);
-    ~Block();
+    Block(size_t x, size_t y, size_t angle, bool isHeavy, size_t heavyCount); // constructor
+    ~Block();          // destructor
     
-    size_t getXcoord();
-    size_t getYcoord();
-    size_t getAngle();
-    bool isHeavy();
-    size_t getHeavyLevel();
-    
-    void setXcoord();
-    void setYcoord();
-    void setAngle();
-    void setHeavy(size_t heavyCount);
-    
+    virtual 
+    size_t getXcoord();            // returns the x coordinate
+    size_t getYcoord();            // returns the y coordinate
+    size_t getAngle();             // returns the current angle rotated
+    bool isHeavy();                // returns if the block is affected by the "Heavy" effect
+    size_t getHowHeavy();          // returns how heavy the block is
+    void setXcoord(size_t x);      // sets the x coordinate
+    void setYcoord(size_t y);      // sets the y coordinate
+    void setAngle(size_t angle);   // sets the current rotated angle
+    void setHeavy(bool isHeavy);   // sets if the current block is heavy or not
+    void setHowHeavy(size_t heavyCount);        // sets how heavy the block is
+    bool isAllCleared();
 };
+
 
 
 #endif
