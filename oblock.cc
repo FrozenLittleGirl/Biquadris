357d2a4ec8b5e57fa9404e7f07638bd12a03f673
Shirley Wang <qianqi.wang@uwaterloo.ca>
2021-07-28 20:58:54 +0000

    Update oblock.cc

--- oblock.cc
+++ oblock.cc
@@ -1,1 +1,49 @@
+#include "oblock.h"
+#include <iostream>
+#include <vector>
+#include <string>
+
+
+Oblock::Oblock(size_t x, size_t y, size_t angle, bool isHeavy, size_t heavyCount, char name): 
+Block(0, 0, 0, isHeavy, heavyCount, 'O') {};
+
+
+
+vector<string> Oblock::getRotateDefault() {
+    vector<string> RotateDefault;
+    RotateDefault.push_back("    ");
+    RotateDefault.push_back("    ");
+    RotateDefault.push_back("OO  ");
+    RotateDefault.push_back("OO  ");
+    return RotateDefault;
+}
+
+vector<string> Oblock::getRotate90() {
+    vector<string> Rotate90;
+    Rotate90.push_back("    ");
+    Rotate90.push_back("    ");
+    Rotate90.push_back("OO  ");
+    Rotate90.push_back("OO  ");
+    return Rotate90;
+}
+
+
+vector<string> Oblock::getRotate180() {
+    vector<string> Rotate180;
+    Rotate180.push_back("    ");
+    Rotate180.push_back("    ");
+    Rotate180.push_back("OO  ");
+    Rotate180.push_back("OO ");
+    return Rotate180;
+}
+
+
+vector<string> Oblock::getRotate270() {
+    vector<string> Rotate270;
+    Rotate270.push_back("    ");
+    Rotate270.push_back("    ");
+    Rotate270.push_back("OO  ");
+    Rotate270.push_back("OO  ");
+    return Rotate270;
+}
 
