17824265b6db9d193718fd5564e0a925340abf7b
Shirley Wang <qianqi.wang@uwaterloo.ca>
2021-07-28 20:58:14 +0000

    Update zblock.cc

--- zblock.cc
+++ zblock.cc
@@ -11,17 +11,17 @@ vector<string> Zblock::getRotateDefault() {
     vector<string> RotateDefault;
     RotateDefault.push_back("    ");
     RotateDefault.push_back("    ");
-    RotateDefault.push_back("zz  ");
-    RotateDefault.push_back(" zz ");
+    RotateDefault.push_back("ZZ  ");
+    RotateDefault.push_back(" ZZ ");
     return RotateDefault;
 }
 
 vector<string> Zblock::getRotate90() {
     vector<string> Rotate90;
     Rotate90.push_back("    ");
-    Rotate90.push_back(" z  ");
-    Rotate90.push_back("zz  ");
-    Rotate90.push_back("z   ");
+    Rotate90.push_back(" Z  ");
+    Rotate90.push_back("ZZ  ");
+    Rotate90.push_back("Z   ");
     return Rotate90;
 }
 
@@ -30,8 +30,8 @@ vector<string> Zblock::getRotate180() {
     vector<string> Rotate180;
     Rotate180.push_back("    ");
     Rotate180.push_back("    ");
-    Rotate180.push_back("zz  ");
-    Rotate180.push_back(" zz ");
+    Rotate180.push_back("ZZ  ");
+    Rotate180.push_back(" ZZ ");
     return Rotate180;
 }
 
@@ -39,9 +39,11 @@ vector<string> Zblock::getRotate180() {
 vector<string> Zblock::getRotate270() {
     vector<string> Rotate270;
     Rotate270.push_back("    ");
-    Rotate270.push_back(" z  ");
-    Rotate270.push_back("zz  ");
-    Rotate270.push_back("z   ");
+    Rotate270.push_back(" Z  ");
+    Rotate270.push_back("ZZ  ");
+    Rotate270.push_back("Z   ");
     return Rotate270;
 }
+
+
 
