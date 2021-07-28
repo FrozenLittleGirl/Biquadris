22a033232eaff0128bb3d01ec97aae8674b6cf5e
Shirley Wang <qianqi.wang@uwaterloo.ca>
2021-07-28 21:09:07 +0000

    Update lblock.h

--- lblock.h
+++ lblock.h
@@ -1,1 +1,21 @@
+#ifndef _LBLOCK_H
+#define _LBLOCK_H
+
+#include "block.h"
+#include <vector>
+#include <string>
+
+class Lblock: public Block {
+    
+    public:
+    Lblock(size_t x, size_t y, size_t angle, bool isHeavy, size_t heavyCount, char name);
+    virtual std::vector<std::string> getRotateDefault() override;
+    virtual std::vector<std::string> getRotate90() override;
+    virtual std::vector<std::string> getRotate180() override;
+    virtual std::vector<std::string> getRotate270() override;
+    
+};
+
+#endif
+
 
