eddd2fe025a835acddef8c307e14bc6b2944d54d
Shirley Wang <qianqi.wang@uwaterloo.ca>
2021-07-28 21:31:03 +0000

    Update tblock.h

--- tblock.h
+++ tblock.h
@@ -1,1 +1,20 @@
+#ifndef _TBLOCK_H
+#define _TBLOCK_H
 
+#include "block.h"
+#include <vector>
+#include <string>
+
+
+class Tblock: public Block {
+    
+    public:
+    Tblock(size_t x, size_t y, size_t angle, bool isHeavy, size_t heavyCount, char name);
+    virtual std::vector<std::string> getRotateDefault() override;
+    virtual std::vector<std::string> getRotate90() override;
+    virtual std::vector<std::string> getRotate180() override;
+    virtual std::vector<std::string> getRotate270() override;
+    
+};
+
+#endif
