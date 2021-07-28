9cd488fb39e5111ba47757ff985a92411acfd2fd
Shirley Wang <qianqi.wang@uwaterloo.ca>
2021-07-28 21:53:28 +0000

    Update jblock.h

--- jblock.h
+++ jblock.h
@@ -1,1 +1,20 @@
+#ifndef _JBLOCK_H
+#define _JBLOCK_H
 
+#include "block.h"
+#include <vector>
+#include <string>
+
+
+class Jblock: public Block {
+    
+    public:
+    Jblock(size_t x, size_t y, size_t angle, bool isHeavy, size_t heavyCount, char name);
+    virtual std::vector<std::string> getRotateDefault() override;
+    virtual std::vector<std::string> getRotate90() override;
+    virtual std::vector<std::string> getRotate180() override;
+    virtual std::vector<std::string> getRotate270() override;
+    
+};
+
+#endif
