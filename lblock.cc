8fc2c50c07ab5a3a5ff00c8d3f0d2214899e860b
Shirley Wang <qianqi.wang@uwaterloo.ca>
2021-07-28 21:07:58 +0000

    Update lblock.cc

--- lblock.cc
+++ lblock.cc
@@ -1,1 +1,49 @@
+#include "lblock.h"
+#include "block.h"
+#include <iostream>
+#include <vector>
+#include <string>
 
+
+Lblock::Lblock(size_t x, size_t y, size_t angle, bool isHeavy, size_t heavyCount, char name): 
+Block(0, 0, 0, isHeavy, heavyCount, 'L') {};
+
+
+
+vector<string> Lblock::getRotateDefault() {
+    vector<string> RotateDefault;
+    RotateDefault.push_back("    ");
+    RotateDefault.push_back("    ");
+    RotateDefault.push_back("  L ");
+    RotateDefault.push_back("LLL ");
+    return RotateDefault;
+}
+
+vector<string> Lblock::getRotate90() {
+    vector<string> Rotate90;
+    Rotate90.push_back("    ");
+    Rotate90.push_back("L   ");
+    Rotate90.push_back("L   ");
+    Rotate90.push_back("LL  ");
+    return Rotate90;
+}
+
+
+vector<string> Lblock::getRotate180() {
+    vector<string> Rotate180;
+    Rotate180.push_back("    ");
+    Rotate180.push_back("    ");
+    Rotate180.push_back("LLL ");
+    Rotate180.push_back("L   ");
+    return Rotate180;
+}
+
+
+vector<string> Lblock::getRotate270() {
+    vector<string> Rotate270;
+    Rotate270.push_back("    ");
+    Rotate270.push_back("LL  ");
+    Rotate270.push_back(" L  ");
+    Rotate270.push_back(" L  ");
+    return Rotate270;
+}
