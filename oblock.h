7ba07887f65c0b673d17a1510f9dfdaa12f30103
Shirley Wang <qianqi.wang@uwaterloo.ca>
2021-07-28 21:00:19 +0000

    Update oblock.h

--- oblock.h
+++ oblock.h
@@ -1,1 +1,22 @@
+#ifndef _OBLOCK_H
+#define _OBLOCK_H
 
+
+#include "block.h"
+#include <iostream>
+#include <vector>
+#include <string>
+
+
+class Oblock: public Block {
+    
+    public:
+    Oblock(size_t x, size_t y, size_t angle, bool isHeavy, size_t heavyCount, char name);
+    virtual std::vector<std::string> getRotateDefault() override;
+    virtual std::vector<std::string> getRotate90() override;
+    virtual std::vector<std::string> getRotate180() override;
+    virtual std::vector<std::string> getRotate270() override;
+    
+};
+
+#endif
