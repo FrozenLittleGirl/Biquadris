6287457735c716c5832d50471764bd678215c10e
Shirley Wang <qianqi.wang@uwaterloo.ca>
2021-07-28 21:46:03 +0000

    Update sblock.h

--- sblock.h
+++ sblock.h
@@ -1,1 +1,19 @@
+#ifndef _SBLOCK_H
+#define _SBLOCK_H
+#include <vector>
+#include <string>
+#include "block.h"
 
+
+class Sblock: public Block {
+    
+    public:
+    Sblock(size_t x, size_t y, size_t angle, bool isHeavy, size_t heavyCount, char name);
+    virtual std::vector<std::string> getRotateDefault() override;
+    virtual std::vector<std::string> getRotate90() override;
+    virtual std::vector<std::string> getRotate180() override;
+    virtual std::vector<std::string> getRotate270() override;
+    
+};
+
+#endif
