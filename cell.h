92321c785ac6ec8208484f34603655a92eca75f1
Shirley Wang <qianqi.wang@uwaterloo.ca>
2021-07-29 03:33:04 +0000

    Update cell.h

--- cell.h
+++ cell.h
@@ -1,1 +1,26 @@
+#ifndef _CELL_H
+#define _CELL_H
+#include <iostream>
+#include <vector>
+#include <string>
 
+
+class Cell {
+    size_t x;
+    size_t y;
+    bool occupied;
+    char name;
+    
+    public:
+    Cell(size_t x, size_t y, bool occupied, char c);
+    size_t getX();
+    size_t getY();
+    void setX(size_t x);
+    void setY(size_t y);
+    bool isOccupied();
+    char getName();
+    void setName(char c);
+    void clearCell();
+};
+
+#endif
