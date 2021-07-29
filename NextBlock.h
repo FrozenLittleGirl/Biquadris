53eb8725d823b9068186017ba660e19675bbd82a
Xiyu Chen <x789chen@uwaterloo.ca>
2021-07-29 05:28:22 +0000

    Add new file

---
+++ NextBlock.h
@@ -0,0 +1,17 @@
+#ifndef _NEXTBLOCK_H_
+#define _NEXTBLOCK_H_
+
+#include "Block.h"
+
+class NextBlock {
+protected:
+	int sd;
+	bool set_sd;
+public:
+	NextBlock(int sd, bool set_sd);
+	// generate a new block
+	virtual shared_ptr<Block> generateBlock() = 0;
+	~NextBlock();
+};
+
+#endif
