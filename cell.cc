d8e56ccd9fa63c37380ec50d5559c9e9af7b4a3b
Shirley Wang <qianqi.wang@uwaterloo.ca>
2021-07-29 03:52:06 +0000

    Update cell.cc

--- cell.cc
+++ cell.cc
@@ -1,1 +1,49 @@
+#include "cell.h"
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
 
+
+Cell::Cell(size_t x, size_t y, bool occupied, char c): 
+x{x}, y{y}, occupied{occupied}, name {c} {}
+
+
+size_t Cell::getX() {
+    return this->x; 
+}
+
+
+size_t Cell::getY() {
+    return this->y; 
+}
+
+
+void Cell::setX(size_t x) {
+    this->x = x;
+}
+
+void Cell::setY(size_t y) {
+    this->y = y;
+}
+
+bool Cell::isOccupied() {
+    return occupied;
+}
+
+
+void Cell::setName(char c) {
+    this->name = c;
+    occupied = true;
+}
+
+
+char Cell::getName() {
+    return name;
+}
+
+
+void Cell::clearCell() {
+    this->name = ' ';
+    occupied = false;
+}
