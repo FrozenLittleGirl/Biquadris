7854cc6d74b010be9ec79595ed94d35545eb9bfc
Shirley Wang <qianqi.wang@uwaterloo.ca>
2021-07-31 06:28:46 +0000

    Update iblock.h

--- iblock.h
+++ iblock.h
@@ -8,7 +8,7 @@
 class Iblock: public Block {
     
     public:
-    Iblock(size_t x, size_t y, size_t angle, bool isHeavy, size_t heavyCount, char name);
+    Iblock(int x, int y, int angle, bool isHeavy, int heavyCount, char name);
     virtual std::vector<std::string> getRotateDefault() override;
     virtual std::vector<std::string> getRotate90() override;
     virtual std::vector<std::string> getRotate180() override;
