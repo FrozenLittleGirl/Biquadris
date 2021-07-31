b3fa96cae061434ee4cd97a69d9624bf76d5900a
Xiyu Chen <x789chen@uwaterloo.ca>
2021-07-31 22:36:38 +0000

    Update board.h

--- board.h
+++ board.h
@@ -28,7 +28,7 @@ class Board, public Subject {
     int score = 0;
     int tmp_score = 0;          // Nata: since there are restart, we need a temporarily score to hold current game's score
     int block_created = 0;
-    bool clear = false;
+    bool clearRow = false;
     bool lose = false;
     //void restore();
     //void addAction(Board* opponent, string s);
