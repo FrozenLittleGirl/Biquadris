f933994d180787b2a41b921b111d72c14322e559
Xiyu Chen <x789chen@uwaterloo.ca>
2021-07-31 06:20:18 +0000

    Update board.h

--- board.h
+++ board.h
@@ -4,14 +4,15 @@
 #include <memory>
 #include <iostream>
 #include "Block.h"
+#include "Cell.h"
 
 class TextDisplay;
 class SpecialAction;
 class NextBlock;
 
 class Board {
-    Block *currentBlock;
-    Block *nextBlock;
+    //Block *currentBlock;  // Nata: have one below
+    //Block *nextBlock;     // Nata: have one below
     vector<vector<Cell>> theBoard;   // Shirley: My implementation used vector<vector<Cell>> instead, is it okay that
                                          // we change it back? Since all the methods such as drop, left, right and so on used 
                                            // vector<vector<Cell>>
@@ -22,12 +23,13 @@ class Board {
     NextBlock* level = nullptr;
     int level_n;
     shared_ptr<Block> block;
+    shared_ptr<Block> nextBlock;
     
     protected:
     int score;
     bool lose = false;
-    void restore();
-    void addAction(Board* opponent, string s);
+    //void restore();
+    //void addAction(Board* opponent, string s);
 
     public: 
         friend &ostream operator<<(std::ostream &out, Board &b);
@@ -39,11 +41,11 @@ class Board {
         void right(int steps);
         void clockwise(int angle);
         void counterclockwise(int angle);
-        bool determineLose();
+        //bool determineLose();
         //void setLoseOrWin(bool);  // Nata: I have a determineLose above
-        int determineScore();    
-        void attach(Board* opponent, int* n);
-        void setRandom(bool set, string s);
+        //int determineScore();    
+        //void attach(Board* opponent, int* n);
+        //void setRandom(bool set, string s);
         void addLevel(int n, int seed, bool set_seed, string file);
         void newBlock(char c = 'n');
         
