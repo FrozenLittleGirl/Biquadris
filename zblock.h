29a13cd4d76c8757420f37c44462ea7a7346f8c2
Shirley Wang <qianqi.wang@uwaterloo.ca>
2021-07-31 06:31:36 +0000

    Update zblock.h

--- zblock.h
+++ zblock.h
@@ -11,7 +11,7 @@
 class Zblock: public Block {
     
     public:
-    Zblock(size_t x, size_t y, size_t angle, bool isHeavy, size_t heavyCount, char name);
+    Zblock(int x, int y, int angle, bool isHeavy, int heavyCount, char name);
     virtual std::vector<std::string> getRotateDefault() override;
     virtual std::vector<std::string> getRotate90() override;
     virtual std::vector<std::string> getRotate180() override;
