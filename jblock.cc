17db29644e83c32b8c5cd30956f9310b5b3418f6
Shirley Wang <qianqi.wang@uwaterloo.ca>
2021-07-31 06:29:00 +0000

    Update jblock.cc

--- jblock.cc
+++ jblock.cc
@@ -6,7 +6,7 @@
 using namespace std;
 
 
-Jblock::Jblock(size_t x, size_t y, size_t angle, bool isHeavy, size_t heavyCount, char name): 
+Jblock::Jblock(int x, int y, int angle, bool isHeavy, int heavyCount, char name): 
 Block(0, 0, 0, isHeavy, heavyCount, 'J') {};
 
 vector<string> Jblock::getRotateDefault() {
