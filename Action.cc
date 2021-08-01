58660a05fac135bad51b2307b561d520330c383e
Xiyu Chen <x789chen@uwaterloo.ca>
2021-08-01 00:40:42 +0000

    Update Action.cc

--- Action.cc
+++ Action.cc
@@ -1,33 +1,14 @@
 #include "Action.h"
-#include "TextDisplay.h"
-#include "Board.h"
 #include <string>
 #include <iostream>
 
 using namespace std;
 
 // Blind
-Blind::Blind(TextDisplay* td, Board* b): td{td}, b{b} {}
-
-void Blind::applyAction() {
-	if (applied == false) {
-		td->notifyBlind();
-	}
-	applied = true;
-}
-
-void Blind::restore() {
-	b->restore();
-}
+void Blind::applyAction() {}
 
 // Heavy
-void Heavy::Heavy(Board* b) : b{ b } {}
-
-void Heavy::applyAction() {
-	b->down(2);
-}
+void Heavy::applyAction() {}
 
 // Force
-
-// don't forget notify
 void Force::applyAction() {}
