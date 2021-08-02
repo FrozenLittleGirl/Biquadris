0b841dcb3c40c7fda6c582913599288dc00f7d4c
Shirley Wang <qianqi.wang@uwaterloo.ca>
2021-08-02 15:22:02 +0000

    Update starblock.h

--- starblock.h
+++ starblock.h
@@ -1,1 +1,19 @@
+#ifndef _STARBLOCK_H
+#define _STARBLOCK_H
 
+#include "block.h"
+#include <vector>
+#include <string>
+
+class starblock: public Block {
+    
+    public:
+    starblock(int x, int y, int angle, bool isHeavy, int heavyCount, char name);
+    virtual std::vector<std::string> getRotateDefault() override;
+    virtual std::vector<std::string> getRotate90() override;
+    virtual std::vector<std::string> getRotate180() override;
+    virtual std::vector<std::string> getRotate270() override;
+    
+};
+
+#endif
