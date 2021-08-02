72b865b101277c25bbb0b97920484b019e1567e1
Xiyu Chen <x789chen@uwaterloo.ca>
2021-08-02 18:41:02 +0000

    Update board.cc

--- board.cc
+++ board.cc
@@ -24,14 +24,15 @@ using namespace std;
 Board::Board() {}
 
 void Board::print() {
-    cout << "....................." << endl;
+    cout << "-----------" << endl;
     for (int i = 0; i < 18; ++i) {
+        cout << "-"
         for (int j = 0; j < 11; ++j) {
             cout << theBoard[i][j].getName();
         }
-        cout << endl;
+        cout << "-" << endl;
     }
-    cout << "....................." << endl;
+    cout << "-----------" << endl;
     vector<string> v = nextBlock->getRotateDefault();
     //cout << v[1] << endl;
     cout << v[2] << endl;
