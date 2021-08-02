d6e9f6097d4ca1ee7e9ec2456d0a9088818f8d00
Xiyu Chen <x789chen@uwaterloo.ca>
2021-08-02 01:58:40 +0000

    Update board.cc

--- board.cc
+++ board.cc
@@ -1,7 +1,9 @@
 #include "SpecialActions.h"
+#include "board.h"
+#include "level.h"
 #include "NextBlock.h"
 #include "Action.h"
-#include "TextDisplay.h"
+#include "textdisplay.h"
 #include <vector>
 #include <string>
 #include <iostream>   // Nata: don't forget to delete this once finished
@@ -12,6 +14,8 @@ using namespace std;
 // for test
 // .................................
 Board::Board() {
+    currentBlock = level->generateBlock();
+    nextBlock = level->generateBlock();
     for (int i = 0; i < 18; ++i) {
         vector<Cell> v;
         theBoard.emplace_back(v);
@@ -43,7 +47,7 @@ void Board::print() {
 
 // a helper to restore the type of cell to ' '
 static void restoreType(shared_ptr<Block> block) {
-    
+
 }
 
 void Board::clearBoard() {
@@ -71,7 +75,7 @@ void Board::init() {
         // initialize each column
         for (int j = 0; j < NUM_COLS; j++) {
             Cell cur{i, j, false, ' '};
-            tmp.push_back(Cell);
+            tmp.push_back(cur);
         }
         // push back each row
         theBoard.push_back(tmp);
@@ -81,100 +85,62 @@ void Board::init() {
 
 bool Board::isShiftValid(int angle, int x, int y) {
     bool is_valid = true;
-    if (angle != 0 || x != 0 || y != 0) {
-        int rotateAngle = currentBlock->getAngle() + angle;
+    int rotateAngle = currentBlock->getAngle() + angle;
         rotateAngle = rotateAngle % 360;
-        if (rotateAngle < 0) {
-            rotateAngle += 360; 
-        }
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                char currentChar = ' ';
-                if (rotateAngle == 0) {
-                    currentChar = currentBlock->getRotateDefault()[i][j];
-                } else if (rotateAngle == 90) {
-                    currentChar = currentBlock->getRotate90()[i][j];
-                } else if (rotateAngle == 180) {
-                    currentChar = currentBlock->getRotate180()[i][j];
-                } else if (rotateAngle == 270) {
-                    currentChar = currentBlock->getRotate270()[i][j];
-                }
-                if (currentChar != ' ') {
-                    int currentAngle = currentBlock->getAngle();
-                    int blockX = currentBlock->getXcoord();
-                    int blockY = currentBlock->getYcoord();
-                    int currentX = currentBlock->getXcoord() + x;
-                    int currentY = currentBlock->getYcoord() + y;
-                    if (currentX + j < 0 || currentY + i < 0 || currentX + j > 19 || currentY + i > 19) {
-                        is_valid = false;
-                    }
-                    if (theBoard[currentY + i][currentX + j].isOccupied() == true) {
-                        if (currentX + j < blockX || currentY + i < blockY || 
-                        currentX + j > blockX + 3 || currentY + i > blockY + 3) {
-                            is_valid = false;
-                        }
-                        if (currentAngle == 0) {
-                            if (currentBlock->getRotateDefault()[y + i][x + j] == ' ') {
-                                is_valid = false;
-                            }
-                        } else if (currentAngle == 90) {
-                            if (currentBlock->getRotate90()[y + i][x + j] == ' ') {
-                                is_valid = false;
-                                
-                            }
-                            
-                        } else if (currentAngle == 180) {
-                            if (currentBlock->getRotate180()[y + i][x + j] == ' ') {
-                                is_valid = false;
-                                
-                            }
-                            
-                        } else if (currentAngle == 270) {
-                            if (currentBlock->getRotate270()[y + i][x + j] == ' ') {
-                                is_valid = false;
-                                
-                            }
-                            
-                        }
-                        
-                    }
-                    
-                }
-                
+    if (rotateAngle < 0) {
+        rotateAngle += 360;
+    }
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            char currentChar = ' ';
+            if (rotateAngle == 0) {
+                currentChar = currentBlock->getRotateDefault()[i][j];
+            } else if (rotateAngle == 90) {
+                currentChar = currentBlock->getRotate90()[i][j];
+            } else if (rotateAngle == 180) {
+                currentChar = currentBlock->getRotate180()[i][j];
+            } else if (rotateAngle == 270) {
+                currentChar = currentBlock->getRotate270()[i][j];
             }
-        }
-    } else {
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
+
+            if (currentChar != ' ') {
                 int currentAngle = currentBlock->getAngle();
                 int blockX = currentBlock->getXcoord();
                 int blockY = currentBlock->getYcoord();
-                char currentChar = ' ';
-                if (currentAngle == 0) {
-                    currentChar = currentBlock->getRotateDefault()[i][j];
-                } else if (currentAngle == 90) {
-                    currentChar = currentBlock->getRotate90()[i][j];
-                } else if (currentAngle == 180) {
-                    currentChar = currentBlock->getRotate180()[i][j];
-                } else if (currentAngle == 270) {
-                    currentChar = currentBlock->getRotate270()[i][j];
+                int currentX = currentBlock->getXcoord() + x;
+                int currentY = currentBlock->getYcoord() + y;
+                if (currentX + j < 0 || currentY + i < 0 || currentX + j > 19 || currentY + i > 19) {
+                    is_valid = false;
                 }
-                if (currentChar != ' ') {
-                    if (theBoard[blockY + i][blockX + j].isOccupied() == true) {
+                if (theBoard[currentY + i][currentX + j].isOccupied() == true) {
+                    if (currentX + j < blockX || currentY + i < blockY ||
+                    currentX + j > blockX + 3 || currentY + i > blockY + 3) {
                         is_valid = false;
-                        this->lose = true;
                     }
-                    if (blockX + j < 0 || blockY + i < 0 || 
-                    blockX + j > 19 || blockY + i > 19) {
-                        is_valid = false;
-                        this->lose = true;
-                        
+                    if (currentAngle == 0) {
+                        if (currentBlock->getRotateDefault()[y + i][x + j] == ' ') {
+                            is_valid = false;
+                        }
+                    } else if (currentAngle == 90) {
+                        if (currentBlock->getRotate90()[y + i][x + j] == ' ') {
+                            is_valid = false;
+                        }
+                    } else if (currentAngle == 180) {
+                        if (currentBlock->getRotate180()[y + i][x + j] == ' ') {
+                            is_valid = false;
+                        }
+                    } else if (currentAngle == 270) {
+                        if (currentBlock->getRotate270()[y + i][x + j] == ' ') {
+                            is_valid = false;
+                        }
                     }
                 }
+
             }
+
         }
     }
-    
     return is_valid;
 }
 
@@ -198,13 +164,13 @@ void Board::move(int angle, int x, int y) {
             if (currentRotation[i][j] != ' ') {
                 theBoard[currentY + i][currentX + j].clearCell();
             }
-            
+
         }
     }
     int rotateAngle = currentAngle + angle;
     rotateAngle = rotateAngle % 360;
     if (rotateAngle < 0) {
-        rotateAngle += 360; 
+        rotateAngle += 360;
     }
     vector<string> rotation;
     if (rotateAngle == 0) {
@@ -226,7 +192,7 @@ void Board::move(int angle, int x, int y) {
             }
         }
     }
-    
+
 }
 
 
@@ -246,7 +212,7 @@ void Board::left(int steps) {
         if (h != nullptr) {
             for (int i = 0; i < 2; ++i) {
                 if (isShiftValid(0, 0, 1) == false) {
-                   drop(); 
+                   drop();
                    break;
                 } else {
                    move(0, 0, 1);
@@ -275,7 +241,7 @@ void Board::right(int steps) {
         if (h != nullptr) {
             for (int i = 0; i < 2; ++i) {
                 if (isShiftValid(0, 0, 1) == false) {
-                   drop(); 
+                   drop();
                    break;
                 } else {
                    move(0, 0, 1);
@@ -337,81 +303,113 @@ void Board::counterclockwise(int angle) {
 
 // for level
 void Board::addLevel(int n, int seed, bool set_seed, string file) {
-	delete level;
+        delete level;
     level_n = n;
-	if (n == 0) {
-		level = new LevelZero{ file, seed, set_seed };
-	}
-	else if (n == 1) {
-		level = new LevelOne{ seed, set_seed };
-	}
-	else if (n == 2) {
-		level = new LevelTwo{ seed, set_seed };
-	}
-	else if (n == 3) {
-		level = new LevelThree{ seed, set_seed };
-	}
-	else {
-		level = new LevelFour{ seed, set_seed };
-	}
+        if (n == 0) {
+                level = new levelZero{ file, seed, set_seed };
+        }
+        else if (n == 1) {
+                level = new levelOne{ seed, set_seed };
+        }
+        else if (n == 2) {
+                level = new levelTwo{ seed, set_seed };
+        }
+        else if (n == 3) {
+                level = new levelThree{ seed, set_seed };
+        }
+        else {
+                level = new levelFour{ seed, set_seed, this };
+        }
+}
+
+// This is a  helper
+Block* helperBlock(int x, int y, char c) {
+  Block * block;
+  if (c == 'L') {
+    block = new Lblock{x, y, 0, false, 0, 'L'};
+  } else if (c == 'I') {
+      block = new Iblock{x, y, 0, false, 0, 'I'};
+  }
+   else if (c == 'J') {
+      block = new Jblock{x, y, 0, false, 0, 'J'};
+  }
+   else if (c == 'O') {
+      block = new Oblock{x, y, 0, false, 0, 'O'};
+  }
+   else if (c == 'S') {
+      block = new Sblock{x, y, 0, false, 0, 'S'};
+  }
+   else if (c == 'Z') {
+      block = new Zblock{x, y, 0, false, 0, 'Z'};
+  }
+   else if (c == 'T') {
+      block = new Tblock{x, y, 0, false, 0, 'T'};
+  }
+   else {
+      block = new Starblock{x, y, 0, false, 0, '*'};
+  }
+  return block;
 }
 
 // for block
 void Board::newBlock(char c) {
     ++block_created;
-	if (c == 'n') {
-		this->currentBlock = level->generateBlock();
-	}
-	else {
+    delete currentBlock;
+        if (c == 'n') {
+                currentBlock = nextBlock;
+                nextBlock = level->generateBlock();
+        }
+        else {
         // Nata: careful! new block should always fit the current position
         --block_created;
         int x = currentBlock->getXcoord();
         int y = currentBlock->getYcoord();
-		this->currentBlock = make_shared<Block>(x, y, 0, false, 0, c);
-	}
+        this->currentBlock = helperBlock(x, y, c);
+        }
     if (block_created % 5 == 0) {
         clearRow = false;
     }
-	nextBlock = level->generateBlock();
     print();
 }
 
 
 // opponent
 void Board::attach(Board* opponent, int* n) {
-	this->opponent = opponent;
-	turn = n;
-	td->attachOpponent(opponent);
+        this->opponent = opponent;
+        turn = n;
+        td->attachOpponent(opponent);
 }
 
 void Board::addAction(Board* opponent, string s) {
     delete opponent->action;
-	cout << "choose an action" << endl;
-	string s;
-	cin >> s;
-	if (s == "blind") {
-		opponent->action = new Blind{ opponent->disp, opponent };
-		opponent->action->applyAction();
-	}
-	else if (s == "heavy") {
-		opponent->action = new Heavy;
-	}
-	else {
-		char c;
-		cin >> c;
-		opponent->action = new Force;
-		opponent->currentBlock = make_shared<Block>(0, 0, 0, false, 0, c);
-		// notify
+        cout << "choose an action" << endl;
+        string in;
+        cin >> in;
+        if (in == "blind") {
+                opponent->action = new Blind;
+                opponent->action->applyAction();
+        }
+        else if (in == "heavy") {
+                opponent->action = new Heavy;
+        }
+        else {
+                char c;
+                cin >> c;
+                opponent->action = new Force;
+                delete opponent->currentBlock;
+                opponent->currentBlock = helperBlock(0, 0, c);
+                // notify
 
-	}
+        }
 }
 
 void Board::dropStar() {
-    shared_ptr<Block> tmp = currentBlock;
-    currentBlock = make_shared<Block>(0, 5, 0, false, 0, '*');
+    Block* tmp = currentBlock;
+    currentBlock = helperBlock(0, 5, '*');
     while (isShiftValid(0, 0, 1) == true) {
         move(0, 0, 1);
     }
+    delete currentBlock;
     detectRow();
     currentBlock = tmp;
 }
@@ -441,20 +439,20 @@ void Board::detectRow() {
         clearRow = true;
     }
     if (count > 1) {
-        addAction();
+        addAction(opponent, " ");
     }
 }
 
 void Board::setRandom(bool set, string s) {
-	level->changeState(set, s);
+        level->changeState(set, s);
 }
 
 bool Board::determineLose() {
-	return lose;
+        return lose;
 }
 
 int Board::determineScore() {
-	return score;
+        return score;
 }
 
 vector<vector<Cell>> Board::getBoard() {
@@ -462,7 +460,7 @@ vector<vector<Cell>> Board::getBoard() {
 }
 
 ostream &operator<<(ostream &out, const Board &b) {
-    cout << b.td->printBoards() << endl;
+    out << *(b.td) << endl;
     return out;
 }
 
@@ -470,6 +468,8 @@ ostream &operator<<(ostream &out, const Board &b) {
 Board::~Board() {
     delete level;
     delete action;
+    delete currentBlock;
+    delete nextBlock;
 }
 
 
