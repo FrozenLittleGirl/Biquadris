86efb139ace9df02dd27504026879bf921c8a28e
Xiyu Chen <x789chen@uwaterloo.ca>
2021-08-02 17:33:14 +0000

    Update board.cc

--- board.cc
+++ board.cc
@@ -339,7 +339,6 @@ void Board::addLevel(int n, int seed, bool set_seed, string file) {
     delete currentBlock;
     delete nextBlock;
     level_n = n;
-    cout << "error addlevel 1" << endl;
     if (n == 0) {
             level = new levelZero{ file, seed, set_seed };
     }
@@ -355,9 +354,7 @@ void Board::addLevel(int n, int seed, bool set_seed, string file) {
     else {
             level = new levelFour{ seed, set_seed, this };
     }
-    cout << "error addlevel 2" << endl;
     currentBlock = level->generateBlock();
-    cout << "error addlevel 3" << endl;
     nextBlock = level->generateBlock();
 }
 
