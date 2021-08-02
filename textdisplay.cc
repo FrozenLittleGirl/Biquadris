ed0dec8bb397c1c4ee7b855f469792bdfdd55601
Kenisha <k4perera@uwaterloo.ca>
2021-08-02 12:27:48 -0400

    fixed textdisplay operator<<

--- textdisplay.cc
+++ textdisplay.cc
@@ -11,32 +11,24 @@ void TextDisplay::attachOpponent(Board* opponent) {
     this->opponent = opponent;
 }
 
-Board* TextDisplay::getBoard() const {
-    return board;
-}
-
-Board* TextDisplay::getOpponent() const {
-    return opponent;
-}
-
 ostream &operator<<(ostream &out, const TextDisplay &td) {
-    out << "Level:    " << td.getBoard()->getLevel() << "      " << "Level:    " << td.getOpponent()->getLevel() << endl;
-    out << "Score:    " << td.getBoard()->getScore() << "      " << "Score:    " << td.getOpponent()->getScore() << endl;
+    out << "Level:    " << td.board->getLevel() << "      " << "Level:    " << td.opponent->getLevel() << endl;
+    out << "Score:    " << td.board->getScore() << "      " << "Score:    " << td.opponent->getScore() << endl;
     out << "-----------" << "      " << "-----------" << endl;
     for (int i = 0; i < NUM_ROWS; i++) {
         for (int j = 0; j < NUM_COLS; j++) {
-            out << td.getBoard()->getBoard()[i][j].getName();
+            out << td.board->getBoard()[i][j].getName();
         }
         out << "      ";
         for (int j = 0; j < NUM_COLS; j++) {
-            out << td.getOpponent()->getBoard()[i][j].getName();
+            out << td.opponent->getBoard()[i][j].getName();
         }
         out << endl;
     }
     out << "-----------" << "      " << "-----------" << endl;
     out << "Next:      " << "      " << "Next:      " << endl;
     for (int i = 0; i < 5; i++) {
-        out << td.getBoard()->getNextBlock()->getRotateDefault()[i] << "            " << td.getOpponent()->getNextBlock()->getRotateDefault()[i] << endl;
+        out << td.board->getNextBlock()->getRotateDefault()[i] << "            " << td.opponent->getNextBlock()->getRotateDefault()[i] << endl;
     }
     return out;
 }
