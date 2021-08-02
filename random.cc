62642b2158657217762d4b95400c836697e1a453
Xiyu Chen <x789chen@uwaterloo.ca>
2021-08-02 02:45:54 +0000

    Update random.cc

--- random.cc
+++ random.cc
@@ -1,11 +1,13 @@
 #include "random.h"
 
-int setChar(double prob1, double prob2, int sd, bool set_sd) {
+using namespace std;
+
+char setChar(double prob1, double prob2, int sd, bool set_sd) {
     static default_random_engine gen;
     static discrete_distribution<int> dist{ prob1, prob1, prob2, prob2, prob2, prob2, prob2 };
-    static bool flag = set_seed;
+    static bool flag = set_sd;
     if (flag) {
-        gen.seed(seed);
+        gen.seed(sd);
         flag = false;
     }
     return chart[dist(gen)];
