249e6bb316cee39966def91df222b79adea54a88
Kenisha <k4perera@uwaterloo.ca>
2021-08-02 15:27:06 -0400

    fixed textdisplay for testing

--- textdisplay.cc
+++ textdisplay.cc
@@ -19,18 +19,22 @@ ostream &operator<<(ostream &out, const TextDisplay &td) {
     out << "Score:    " << td.board->getScore() << "      " << "Score:    " << td.opponent->getScore() << endl;
     out << "-----------" << "      " << "-----------" << endl;
     for (int i = 0; i < td.NUM_ROWS; i++) {
+        cout << "-";
         for (int j = 0; j < td.NUM_COLS; j++) {
             out << td.board->getBoard()[i][j].getName();
         }
+        cout << "-";
         out << "      ";
+        cout << "-";
         for (int j = 0; j < td.NUM_COLS; j++) {
             out << td.opponent->getBoard()[i][j].getName();
         }
+        cout << "-";
         out << endl;
     }
     out << "-----------" << "      " << "-----------" << endl;
     out << "Next:      " << "      " << "Next:      " << endl;
-    for (int i = 0; i < 5; i++) {
+    for (int i = 2; i < 5; i++) {
         out << td.board->getNextBlock()->getRotateDefault()[i] << "            " << td.opponent->getNextBlock()->getRotateDefault()[i] << endl;
     }
     return out;
