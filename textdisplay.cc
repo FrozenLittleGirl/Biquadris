3bb02f6330a52dfe9f4d1f9028e5c370fa4a1b6e
Kenisha <k4perera@uwaterloo.ca>
2021-08-02 17:26:21 -0400

    fixed textdisplay

--- textdisplay.cc
+++ textdisplay.cc
@@ -20,7 +20,7 @@ ostream &operator<<(ostream &out, const TextDisplay &td) {
     out << "Level:    " << td.board->getLevel() << "      " << "Level:    " << td.opponent->getLevel() << endl;
     out << "Score:    " << td.board->getScore() << "      " << "Score:    " << td.opponent->getScore() << endl;
     out << "-----------" << "      " << "-----------" << endl;
-    if ( dynamic_cast<Blind*>(board->getAction()) ) {
+    if ( dynamic_cast<Blind*>(td.board->getAction()) ) {
         for (int i = 0; i < td.NUM_ROWS; i++) {
             cout << "-";
             for (int j = 0; j < td.NUM_COLS; j++) {
@@ -38,10 +38,10 @@ ostream &operator<<(ostream &out, const TextDisplay &td) {
             cout << "-";
             out << endl;
         }        
-    } else if ( dynamic_cast<Blind*>(opponent->getAction()) ) {
+    } else if ( dynamic_cast<Blind*>(td.opponent->getAction()) ) {
         for (int i = 0; i < td.NUM_ROWS; i++) {
             cout << "-";
-            for (int j = 0; j < td.NUM_COLS; j++) {}
+            for (int j = 0; j < td.NUM_COLS; j++) {
                 out << td.board->getBoard()[i][j].getName();                
             }
             cout << "-";
