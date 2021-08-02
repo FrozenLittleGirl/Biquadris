e24f57c800814d4327d4ade9da97a0c881923a8c
Xiyu Chen <x789chen@uwaterloo.ca>
2021-08-02 02:44:38 +0000

    Update NextBlock.h

--- NextBlock.h
+++ NextBlock.h
@@ -1,18 +1,20 @@
 #ifndef _NEXTBLOCK_H_
 #define _NEXTBLOCK_H_
+#include <string>
+#include "block.h"
 
-#include "Block.h"
+using namespace std;
 
 class NextBlock {
 protected:
-	int sd;
-	bool set_sd;
+        int sd;
+        bool set_sd;
 public:
-	NextBlock(int sd, bool set_sd);
-	// generate a new block
-	virtual shared_ptr<Block> generateBlock() = 0;
-	virtual void changeState(bool set, string s) = 0;
-	~NextBlock();
+        NextBlock(int sd, bool set_sd);
+        // generate a new block
+        virtual Block* generateBlock() = 0;
+        virtual void changeState(bool set, string s) = 0;
+        virtual ~NextBlock();
 };
 
 #endif
