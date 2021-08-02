87d360defd89b3793b64dc082a24355d04f40751
Xiyu Chen <x789chen@uwaterloo.ca>
2021-08-02 02:45:34 +0000

    Update level.h

--- level.h
+++ level.h
@@ -1,7 +1,6 @@
 #ifndef _LEVEL_H_
 #define _LEVEL_H_
 #include "NextBlock.h"
-#include <memory>
 #include <vector>
 
 using namespace std;
@@ -9,57 +8,57 @@ using namespace std;
 class Board;
 
 class levelZero : public NextBlock {
-	vector<char> sequence;
-	int size = 0;
-	int current = 0;
+        vector<char> sequence;
+        int size = 0;
+        int current = 0;
 public:
-	levelZero(string file, int sd, bool set_sd);
-	shared_ptr<Block> generateBlock() override;
-	void changeState(bool set, string s) override;
+        levelZero(string file, int sd, bool set_sd);
+        Block* generateBlock() override;
+        void changeState(bool set, string s) override;
 };
 
 class levelOne : public NextBlock {
-	double probSZ = 1;
-	double probOther = 2;
+        double probSZ = 1;
+        double probOther = 2;
 public:
-	levelOne(int sd, bool set_sd);
-	shared_ptr<Block> generateBlock() override;
-	void changeState(bool set, string s) override;
+        levelOne(int sd, bool set_sd);
+        Block* generateBlock() override;
+        void changeState(bool set, string s) override;
 };
 
 class levelTwo : public NextBlock {
-	double prob = 1;
+        double prob = 1;
 public:
-	levelTwo(int sd, bool set_sd);
-	shared_ptr<Block> generateBlock() override;
-	void changeState(bool set, string s) override;
+        levelTwo(int sd, bool set_sd);
+        Block* generateBlock() override;
+        void changeState(bool set, string s) override;
 };
 
 class levelThree : public NextBlock {
-	double probSZ = 2;
-	double probOther = 1;
-	bool random = true;
-	vector<char> sequence;
-	int size = 0;
-	int current = 0;
+        double probSZ = 2;
+        double probOther = 1;
+        bool random = true;
+        vector<char> sequence;
+        int size = 0;
+        int current = 0;
 public:
-	levelThree(int sd, bool set_sd);
-	shared_ptr<Block> generateBlock() override;
-	void changeState(bool set, string s) override;
+        levelThree(int sd, bool set_sd);
+        Block* generateBlock() override;
+        void changeState(bool set, string s) override;
 };
 
 class levelFour : public NextBlock {
-	double probSZ = 2;
-	double probOther = 1;
-	bool random = true;
-	vector<char> sequence;
-	int size = 0;
-	int current = 0;
-	Board* b;
+        double probSZ = 2;
+        double probOther = 1;
+        bool random = true;
+        vector<char> sequence;
+        int size = 0;
+        int current = 0;
+        Board* b;
 public:
-	levelFour(int sd, bool set_sd, Board* b);
-	shared_ptr<Block> generateBlock() override;
-	void changeState(bool set, string s) override;
+        levelFour(int sd, bool set_sd, Board* b);
+        Block* generateBlock() override;
+        void changeState(bool set, string s) override;
 };
 
 #endif
