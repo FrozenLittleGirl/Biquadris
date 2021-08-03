136122fadba9a047e14eb43ec572e75868f982f3
Shirley Wang <qianqi.wang@uwaterloo.ca>
2021-08-03 20:40:24 +0000

    Update board.h

--- board.h
+++ board.h
@@ -62,8 +62,6 @@ class Board {
         Block* getNextBlock() const;
         int getLevel() const;
         int getScore() const;
-        Block *getCurrentBlock();
-        void setCurrentBlock();
         SpecialAction* getAction() const;
         vector<vector<Cell>> getBoard() const;
         void print();   // Nata: this is for temporarily testing
