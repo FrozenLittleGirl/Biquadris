7cbc609a6531eee427ef355239f238aef067ae4f
Xiyu Chen <x789chen@uwaterloo.ca>
2021-08-04 05:47:26 +0000

    Update cell.cc

--- cell.cc
+++ cell.cc
@@ -96,11 +96,11 @@ void Cell::clearCell() {
     level_n = 0;
 }
 
-void Cell:setLevel(int level_n) {
+void Cell::setLevel(int level_n) {
     this->level_n = level_n;
 }
 
-int Cell:getLevel() {
+int Cell::getLevel() {
     return level_n;
 }
 
