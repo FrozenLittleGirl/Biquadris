c85159b7c9897ea1c3d6295343ec8ba6a7588685
Kenisha Perera <kenisha.perera@uwaterloo.ca>
2021-08-05 20:29:46 +0000

    Update cell.cc

--- cell.cc
+++ cell.cc
@@ -49,9 +49,9 @@ void Cell::display(int x_coord, int y_coord, bool isBlind) {
         return;
     }
     if ( isBlind ) {
-        view->fillCell(graphicx + x_coord, graphicy + y_coord, 30, Xwindow::Black);
+        view->fillCell(graphicx + x_coord, graphicy + y_coord, 27, Xwindow::Black);
     } else {
-        view->fillCell(graphicx + x_coord, graphicy + y_coord, 30, colour);
+        view->fillCell(graphicx + x_coord, graphicy + y_coord, 27, colour);
     }
 }
 
