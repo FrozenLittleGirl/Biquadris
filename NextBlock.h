553609ca0a30602f4fccd8df447d082630ecea38
Xiyu Chen <x789chen@uwaterloo.ca>
2021-08-12 21:05:33 +0000

    Update NextBlock.h

--- NextBlock.h
+++ NextBlock.h
@@ -10,9 +10,11 @@ protected:
         int sd;
         bool set_sd;
 public:
+        // initialzie the block with seed if set_seed is true
         NextBlock(int sd, bool set_sd);
         // generate a new block
         virtual Block* generateBlock() = 0;
+        // convert level 3 or 4 to random/nonrandom based on bool set
         virtual void changeState(bool set, string s) = 0;
         virtual ~NextBlock();
 };
