0d1e9d5c312790d58a98134c03dc225be1ef3ef9
Xiyu Chen <x789chen@uwaterloo.ca>
2021-08-12 21:19:17 +0000

    Update Action.h

--- Action.h
+++ Action.h
@@ -5,17 +5,17 @@
 
 class Blind : public SpecialAction {
 public:
-        void applyAction() override;
+        void applyAction() override;  // really does nothing at current level of implementation
 };
 
 class Heavy : public SpecialAction {
 public:
-        void applyAction() override;
+        void applyAction() override;  // really does nothing at current level of implementation
 };
 
 class Force : public SpecialAction {
 public:
-        void applyAction() override;
+        void applyAction() override;  // really does nothing at current level of implementation
 };
 
 #endif
