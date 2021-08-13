e938d9a4f0e40a507538e56ceceb407f0bdc3b9f
Kenisha <k4perera@uwaterloo.ca>
2021-08-13 12:29:54 -0400

    update

--- board.cc
+++ board.cc
@@ -698,7 +698,10 @@ void Board::detectRow() {
                 }
                 for (int i = row - 1; i >= 3; --i) {
                     for (int j = 0; j < 11; ++j) {
-                        theBoard[i + 1][j] = theBoard[i][j];
+                        bool occupied = theBoard[i][j].getOccupied();
+                        char name = theBoard[i][j].getName();
+                        theBoard[i + 1][j].setName(name);
+                        theBoard[i + 1][j].setOccupied(occupied);
                         theBoard[i + 1][j].setColour();
                     }
                 }
@@ -744,7 +747,11 @@ void Board::detectRow() {
     }
 
     if (determineClear) {
-
+        for (int i = startRow; i >= 4; i--) {
+            for (int j = 0; j < 11; j++) {
+                displayCell(i, j, theBoard);
+            }
+        }
     }
 
     displayScore();
